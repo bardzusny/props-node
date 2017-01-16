f310cf67a8dba965803be56faec41c222b3bb833
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-16 18:25:49 +0100

    GET /users: basic test cases

--- test/api/controllers/users.spec.js
+++ test/api/controllers/users.spec.js
@@ -6,12 +6,59 @@ const UserFactory = require('../../factories/user');
 const { User } = require('../../../models');
 const jwt = require('jwt-simple');
 const sinon = require('sinon');
+const authHelper = require('../../helpers/auth_helper');
 
 describe('controllers', () => {
+  let token;
+  let user;
+
   describe('users', () => {
     before((done) => {
+      process.env.JWT_TOKEN = '123';
+      const password = chance.string();
+
       User.destroy({ where: {} })
-        .then(() => done());
+        .then(() => UserFactory.create(password))
+        .then((createdUser) => {
+          user = createdUser;
+          return authHelper.fetchAuthorizationToken(user, password);
+        }).then((authToken) => {
+          token = authToken;
+          done();
+        });
+    });
+
+    describe('GET /users', () => {
+      describe('without correct authentication header', () => {
+        it('should return 401 unauthorized', (done) => {
+          request(server)
+            .get('/api/users')
+            .send({ username: chance.name() })
+            .expect(401)
+            .end((err) => {
+              should.not.exist(err);
+              done();
+            });
+        });
+      });
+
+      describe('with correct authentication header', () => {
+        it('should respond with list of users', (done) => {
+          request(server)
+            .get('/api/users')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+            .end((err, res) => {
+              should.not.exist(err);
+
+              User.findAll({})
+                .then((users) => {
+                  res.body.length.should.eql(users.length);
+                  done();
+                });
+            });
+        });
+      });
     });
 
     describe('POST /users', () => {
@@ -45,8 +92,8 @@ describe('controllers', () => {
               res.body.password.should.not.eql(userData.password);
 
               User.find({ where: { id: res.body.id } })
-                .then((user) => {
-                  should.exist(user);
+                .then((registeredUser) => {
+                  should.exist(registeredUser);
                   done();
                 });
             });
@@ -56,11 +103,11 @@ describe('controllers', () => {
 
     describe('POST /users/login', () => {
       const password = chance.string();
-      let user;
+      let registeredUser;
 
       before((done) => {
         UserFactory.create(password).then((createdUser) => {
-          user = createdUser;
+          registeredUser = createdUser;
           done();
         });
       });
@@ -89,7 +136,7 @@ describe('controllers', () => {
         it('should return 401 for wrong password', (done) => {
           request(server)
             .post('/api/users/login')
-            .send({ username: user.username, password: `${user.password}1` })
+            .send({ username: registeredUser.username, password: `${registeredUser.password}1` })
             .expect(401)
             .end(() => done());
         });
@@ -107,12 +154,12 @@ describe('controllers', () => {
         it('should return authentication token with user ID', (done) => {
           request(server)
             .post('/api/users/login')
-            .send({ username: user.username, password })
+            .send({ username: registeredUser.username, password })
             .expect(200)
             .end((err, res) => {
               should.not.exist(err);
 
-              jwt.encode.calledWith({ id: user.id }).should.be.ok;
+              jwt.encode.calledWith({ id: registeredUser.id }).should.be.ok;
               res.body.should.eql({ token: 'I am Mr. Token' });
 
               done();
