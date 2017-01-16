51b6fe52c18aaccb163045b4c7a47e3fa53e08c3
Adrian Zalewski <aazalewski@hotmail.com>
2017-01-16 19:53:05 +0100

    Prop test factory: handling propsed users (belongsToMany association)

--- test/factories/prop.js
+++ test/factories/prop.js
@@ -2,10 +2,15 @@ const { Prop } = require('../../models');
 const chance = require('chance').Chance();
 
 module.exports = {
-  create(author) {
+  create(author, propsed) {
+    let prop;
     return Prop.create({
       body: chance.string(),
       UserId: author.id,
-    });
+    }).then((createdProp) => {
+      prop = createdProp;
+      // TODO: find a way to add belongsToMany associations within .create() call?
+      return prop.addPropsed(propsed);
+    }).then(() => prop);
   },
 };
